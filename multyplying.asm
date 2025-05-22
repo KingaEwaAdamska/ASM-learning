74a4f93e62096de08f0aebe4c7ee4d5a55f4bbbb
KingaEwaAdamska <kingaewa05@gmail.com>
2025-05-22 17:51:31 +0200

    chart and comments

--- multyplying.asm
+++ multyplying.asm
@@ -1,7 +1,7 @@
 .data
 
 result:  	.word 0	  # wynik
-overflow_msg:   .asciiz "Result overflowed"
+overflow_msg:   .asciiz "Result overflowed" 
 prompt1:        .asciiz "Enter multiplicand: "
 prompt2:        .asciiz "Enter multiplier:   "
 
@@ -10,7 +10,7 @@ prompt2:        .asciiz "Enter multiplier:   "
 # $t0 - mnożna
 # $t1 - mnożnik
 # $t2 - wynik
-# $t3 - bufor dla ostatniej cyfry mnożnika
+# $t3 - ostatni bit mnożnika
 
 main:
 	# wyświetlenie prompt1 - prośy o mnożną
@@ -42,25 +42,29 @@ loop:
 	beqz $t3, skip # jesli ostatnia cyfra mnożnika 0 - idź dalej 
 	
 	addu $t4, $t0, $t2 # do dodatkowego rejestru dodajemy wynik
-	blt $t4, $t2, overflow
-	move $t2, $t4
+	blt $t4, $t2, overflow # sprawdzenie czy nie nastąpił nadmiar iewentualne zwrócenie informacji
+	move $t2, $t4 # przepisanie wyniku w przypadku braku nadmiaru
 	
 skip:
-	sll $t0, $t0, 1
-	srl $t1, $t1, 1
+	sll $t0, $t0, 1 # przesunięcie mnożnej w lewo o 1 cyfrę
+	srl $t1, $t1, 1 # przesunięcie mnożnika w prawo o 1 cyfrę
 	j loop
 end:
-	li $v0, 1
+	# wyświetlenie wartości
+	li $v0, 1 
 	move $a0, $t2
 	syscall
 	
+	# zakończenie programu
 	li $v0, 10
 	syscall
 
 overflow:
+	# wyświetlenie komunikatu
 	li $v0, 4
 	la $a0, overflow_msg
 	syscall
 	
+	# zakończenie programu
 	li $v0, 10
 	syscall
