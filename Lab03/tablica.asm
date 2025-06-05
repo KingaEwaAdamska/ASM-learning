6aeb8ada87ef87c72c2c896b5becfb80ee88b362
KingaEwaAdamska <kingaewa05@gmail.com>
2025-06-05 17:40:02 +0200

    ready code before cleaning

--- Lab03/tablica.asm
+++ Lab03/tablica.asm
@@ -3,6 +3,11 @@
 RAM:                .space 4096  # Zakładam, że moja tablica nie przekroczy 4096 bajtów
 wierszeText:        .asciiz "Podaj ilosc wierszy: "
 kolumnyText:        .asciiz "Podaj ilosc kolumn: "
+operacjaText: 		.asciiz "Wybierz operacje (o - odczyt, z - zapis, w - wyjscie): "
+wierszText:        .asciiz "Podaj wiersz: "
+kolumnaText:        .asciiz "Podaj kolumne: "
+odczytText: 		.asciiz "Odczytana wartosc: "
+zapisText: 			.asciiz "Wartość do zapisania: "
 newline:            .asciiz "\n"
 space:              .asciiz " "
 
@@ -83,19 +88,96 @@ create_loop:
     
 end_create_loop:
 
-	# Zapisuję początkowy adres pierwszego poziomu tablicy:
-	move $t0, $s3
-	sll $t1, $t1, 2 # Wyznaczam ilość bajtów, o którą muszę przesunąć adres $s0, by uzyskać początek drugiego poziomu tablicy
-    add $t2, $s3, $t1 # Teraz $t2 zawiera początkowy adres drugiego poziomu tablicy
-fill_loop_outer:
-	fill_loop_inner:
+user_loop:
+########## Wybór operacji #########
+	la $a0, operacjaText
+	li $v0, 4
+	syscall
+	
+	li $v0, 12
+	syscall
+	
+	move $t0, $v0
+	
+	la $a0, newline
+	li $v0, 4
+	syscall
+	
+	beq $t0, 'w', end_user_loop
+
+########## Wybór wiersza #########
+	la $a0, wierszText
+	li $v0, 4
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	move $t1, $v0
+	
+########## Wybór kolumny #########
+	la $a0, kolumnaText
+	li $v0, 4
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	move $t2, $v0
+
+########## Wykonanie operacji #########
+	sll $t3, $t1, 2 # wyznaczam ilość bajtów do przesunięcia by dotrzeć do dobrego adresu poziomu 1
+	add $t4, $s3, $t3 # przechodzę pod odpowiedni adres, by być w dobrym wierszu
+	
+	lw $t5, ($t4)
+	sll $t3, $t2, 2 # wyznaczam ilośc bajtów do przesunięcia aby dotrzeć do dobrzej kolumny
+	add $t5, $t5, $t3
+	
+	beq $t0, 'z', zapis
+	beq $t0, 'o', odczyt
+	
+	la $a0, newline
+	li $v0, 4
+	syscall
+	
+	j user_loop
+	
+
+odczyt:
+	
+	la $a0, odczytText
+	li $v0, 4
+	syscall
+	
+	li $v0, 1
+	lw $a0, ($t5)
+	syscall
+	
+	move $t2, $v0
+	
+	la $a0, newline
+	li $v0, 4
+	syscall
+
+	j user_loop
+
+zapis:
+
+	la $a0, zapisText
+	li $v0, 4
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	move $t6, $v0
+	
+	sw $t6, ($t5)
+
+	j user_loop
 
 
-	end_fill_loop_inner:
-end_fill_loop_outer:
-
-
-
+end_user_loop:
 
 # WYSWIETLANIE PEŁNEJ TABLICY
 move $t0, $s3        # wskaźnik do tablicy wskaźników (poziom 1)
